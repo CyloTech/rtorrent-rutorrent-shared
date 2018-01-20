76a321ff630e8f7c5555db7a5888035ad0b0aa22
Rid <grant@seednet.eu>
2018-01-20 19:07:22 +0000

    Updated configs

--- startup-rtorrent.sh
+++ startup-rtorrent.sh
@@ -29,6 +29,8 @@ chown -R rtorrent:rtorrent /torrents/config/log/rtorrent
 
 rm -f /torrents/config/rtorrent/session/rtorrent.lock
 
+sed -i 's#http://mydomain.com#'${EXTERNAL_DOMAIN}'/no-auth#g' /var/www/rutorrent/plugins/fileshare/share.php
+
 # run
 su --login --command="TERM=xterm rtorrent" rtorrent 
 
